b48627a695eb0a43798bffb39c2110f41fde36a7
neeeeeek <neeeeeek@users.noreply.github.com>
2018-07-25 20:47:37 +0200

    added_test_DB

---
+++ FOODIE/FOODIE/TestDB.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace FOODIE
+{
+    public partial class TestDB : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["TestConnectionString"].ConnectionString);
+                con.Open();
+                string insert = "insert into userinfo (name, number) values(@name, @number)";
+                SqlCommand cmd = new SqlCommand(insert, con);
+                cmd.Parameters.AddWithValue("@name", TextBox1.Text);
+                cmd.Parameters.AddWithValue("@number", TextBox2.Text);
+                cmd.ExecuteNonQuery();
+                Response.Redirect("test.aspx");
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex);
+            }
+
+        }
+    }
+}
